7614e33bcc128dafecf0c0600ab247db366a9084
Esraa Elkabeer <esraaelkaber18@gmail.com>
2021-01-23 22:12:29 +0200

    update cifar_10

--- DLFrameWork/dataset/CIFAR_10.py
+++ DLFrameWork/dataset/CIFAR_10.py
@@ -16,6 +16,10 @@ class CIFAR_10:
         if self.download:
             self._Download()
         self.path = os.getcwd() + '/' + self.path
+        self.toCSV()
+
+        self.TrainFile = self.path + '/' + 'cifar-10-batches-py/train_cifar.csv'
+        self.TestFile = self.path + '/' + 'cifar-10-batches-py/test_batch.csv'
 
     def _Download(self):
         if not os.path.exists(os.getcwd() + '/' + self.path):
@@ -35,17 +39,26 @@ class CIFAR_10:
         return dict
 
     def toCSV(self):
-        file_names = ['data_batch_1', 'data_batch_2', 'data_batch_3', 'data_batch_4', 'data_batch_5']
+        file_names = ['data_batch_1', 'data_batch_2', 'data_batch_3', 'data_batch_4', 'data_batch_5', 'test_batch']
         for name in file_names:
             df_labels = pd.DataFrame(self.unpickle(self.path + '/' + 'cifar-10-batches-py/' + name)[b'labels'])
             df_data = pd.DataFrame(self.unpickle(self.path + '/' + 'cifar-10-batches-py/' + name)[b'data'])
             new = pd.concat([df_labels, df_data], axis=1)
-            new.to_csv(self.path + '/' + 'cifar-10-batches-py/' + name + '.csv', index=False)
+            if not os.path.exists(self.path + '/' + 'cifar-10-batches-py/' + name + '.csv'):
+                new.to_csv(self.path + '/' + 'cifar-10-batches-py/' + name + '.csv', index=False)
 
-        for name in file_names:
+        for name in file_names[0:5]:
             self.csv_list.append(self.path + '/' + 'cifar-10-batches-py/' + name + '.csv')
 
+        if not os.path.exists(self.path + '/' + 'cifar-10-batches-py/train_cifar.csv'):
+            df_from_each_file = (pd.read_csv(f, sep=',', header=None) for f in self.csv_list)
+            df_merged = pd.concat(df_from_each_file, ignore_index=True)
+            df_merged.to_csv(self.path + '/' + 'cifar-10-batches-py/train_cifar.csv', index=False)
 
-CIFAR10_data = CIFAR_10('blah', False, True)
-CIFAR10_data.toCSV()
-print(CIFAR10_data.csv_list)
+    def __repr__(self):
+        return self.TrainFile if self.train == True else self.TestFile
+
+
+CIFAR10_data = CIFAR_10('blah', False, False)
+# CIFAR10_data.toCSV()
+print(CIFAR10_data)
