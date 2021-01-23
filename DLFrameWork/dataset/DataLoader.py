9c6bd6de0cb88dc443657813f4f900b2714d0475
Mostafa-Ashraf19 <mostaphaashraf19@gmail.com>
2021-01-24 01:05:46 +0200

    dataiterable

--- DLFrameWork/dataset/DataLoader.py
+++ DLFrameWork/dataset/DataLoader.py
@@ -1,8 +1,18 @@
 import pandas
 import numpy as np
 
+class DataLoaderIterator:
+    def __init__(self,dataloader):
+        self.dataloader = dataloader
+        self.index = 0
+    def __next__(self):
+        if self.index < self.dataloader.iternums:
+            result = self.dataloader.__next__() 
+            self.index +=1
+            return result[0],result[1] 
+        raise StopIteration
+        
 class DataLoader:
-
     def __init__(self,path='',batchsize=20,shuffling=True, normalization={}):
         """
         initializing the params
@@ -14,39 +24,37 @@ class DataLoader:
         @return: 1) features raws
                 2) labels raws
         """
-        self.path = path
+        self.path = str(path)
         self.current_position = 0
         self.batchsize = batchsize
         self.shuffling = shuffling
         self.normalization = normalization['Transform'] if type(normalization['Transform']) == bool else RuntimeError('should be bool value ') 
         # self.transform = transform
+        self.iternums = 5
 
-
-    def __itr__(self):
-        return self
-
+    def __iter__(self):
+           return DataLoaderIterator(self)  #[1,2,4,5,6,7], [11,11,11,11,11,11]
     def __next__(self):
-           
             df = pandas.read_csv(self.path, skiprows=self.current_position,nrows=self.batchsize)  
             self.current_position += self.batchsize
 
             if self.normalization == False:             #if No normalization
 
                 if not self.shuffling:                  #if No shuffling
-                    return df.iloc[:, 1:].to_numpy(), df.iloc[:, 0].to_numpy()  
+                    return df.iloc[:, 1:].to_numpy(), df.iloc[:, 0].to_numpy().reshape(-1,1)
 
                 else:                                   #if shuffling
                     x = df.iloc[:, 1:].to_numpy()
                     y = df.iloc[:, 0].to_numpy()
                     np.random.shuffle(x)
                     np.random.shuffle(y)
-                    return x , y
+                    return x , y.reshape(-1,1)
                 
             else:                                       #if normalization
                 if not self.shuffling:                  #if No shuffling
                     x = df.iloc[:, 1:].to_numpy()
                     norm_x = (x - np.min(x))/np.ptp(x)
-                    return norm_x , df.iloc[:, 0].to_numpy()
+                    return norm_x , df.iloc[:, 0].to_numpy().reshape(-1,1)
 
                 else:                                   #if shuffling
                     x = df.iloc[:, 1:].to_numpy()
@@ -54,7 +62,7 @@ class DataLoader:
                     y = df.iloc[:, 0].to_numpy()
                     np.random.shuffle(norm_x)
                     np.random.shuffle(y)
-                    return norm_x , y
+                    return norm_x , y.reshape(-1,1)
 
 
 
