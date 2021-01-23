5dbbe80bf582d676b590856c59112f3489c023ee
Mostafa-Ashraf19 <mostaphaashraf19@gmail.com>
2021-01-23 17:43:54 +0200

    init file

--- DLFrameWork/losses/__init__.py
+++ DLFrameWork/losses/__init__.py
@@ -1,3 +1,4 @@
 from .BiploarPerceptron import BiploarPerceptron 
 from .SD import SD 
 from .Biploar_SVM import Biploar_SVM 
+from .last import multi_class_SVM,multi_class_perceptron,softmax_cross_entropy
