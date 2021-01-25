e647368dab8c6da5baa0912bf437599ebfda8f0e
Mostafa-Ashraf19 <mostaphaashraf19@gmail.com>
2021-01-25 07:23:21 +0200

    SoftmaxCrossEntropy

--- DLFrameWork/losses/SoftmaxCrossEntropy.py
+++ DLFrameWork/losses/SoftmaxCrossEntropy.py
@@ -1,7 +1,4 @@
-#from ..forward import Sigmoid,ReLU,Tanh,Linear,Identity 
 import numpy as np
-# from MultiClassSVM  import *
-# from MultiPerceptron import *
 
 class SoftmaxCrossEntropy:
     def  __init__ (self):
@@ -21,22 +18,7 @@ class SoftmaxCrossEntropy:
 
         return cost
        
-        
-    # @staticmethod
-    # def _Grad(prediction,label) :   
-    #     dl_dy=np.zeros_like(prediction)
 
-    #     for i in range (label.shape[1]):
-
-    #         ind=label[0,i]
-            
-    #         y_label=prediction[ind][i]
-            
-    #         dl_dy[:,i]=-1/y_label
-
-        
-
-    #     return   dl_dy  
         
     @staticmethod
     def _Grad(prediction,label) :
@@ -51,49 +33,3 @@ class SoftmaxCrossEntropy:
 
         return   dl_dy
 
-
-    # @staticmethod
-    # def _Cost(prediction,label):
-
-    #     lossval=0
-
-    #     for i in range (label.shape[1]):
-
-    #         ind=label[0,i]
-    #         y_label=prediction[ind][i]
-    #         lossval+=-np.log(y_label)
-
-    #     cost = lossval/label.shape[1]
-
-    #     return cost
-        
-      
-        
-    # @staticmethod
-    # def _Grad(prediction,label) :
-        
-        
-    #     dl_dy=np.zeros_like(prediction)
-
-    #     for i in range (label.shape[1]):
-
-    #         ind=label[0,i]
-            
-    #         y_label=prediction[ind][i]
-            
-    #         dl_dy[:,i]=-1/y_label
-
-        
-
-    #     return   dl_dy
-    
-            
-
-                
-            
-                 
-       
-        
-           
-    
-       
