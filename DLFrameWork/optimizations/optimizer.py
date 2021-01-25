62b4fa10e2aac6f21e6cecc91fb17409e56d8d1f
Mostafa-Ashraf19 <mostaphaashraf19@gmail.com>
2021-01-25 07:24:13 +0200

    optimizer

--- DLFrameWork/optimizations/optimizer.py
+++ DLFrameWork/optimizations/optimizer.py
@@ -9,19 +9,16 @@ class Optimizer:
         self.parameters = parameters
 
     def normalGD(self):
-        L = self.LayersLen #   # number of layers in the neural network
+        L = self.LayersLen  # number of layers in the neural network
         # Update rule for each parameter. Use a for loop.
         for l in range(L):
             self.parameters["W" + str(l+1)] = self.parameters["W" + str(l+1)] - self.lr * self.grads['dW'+str(l+1)]
             self.parameters["b" + str(l+1)] = self.parameters["b" + str(l+1)] - self.lr * self.grads['db'+str(l+1)]
-        # grads.clear()
         return self.parameters     
-        # pass
         
     def optimize(self):
             if 'GD' in self.optimiType and self.optimiType['GD']:
                 return self.normalGD() # return new paramters 
-                # return Backward.StaticParamUpdate(self.LayersLen,parameters=self.parameters,grads=grads,learning_rate=lr)
             elif 'Adam' in self.optimiType and self.optimiType['Adam']:
                 v,s = Adam.initialize_adam(parameters=self.parameters,layerlen=self.LayersLen)
                 return Adam.update_parameters(layerlen=self.LayersLen,v=v,s=s,grads=self.grads,parameters=self.parameters,learning_rate=self.lr)
