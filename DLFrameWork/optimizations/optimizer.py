1ed896b689a97c4b4df99f86708d314d6215d1c1
Mostafa-Ashraf19 <mostaphaashraf19@gmail.com>
2021-01-25 06:53:44 +0200

    optimizer update

--- DLFrameWork/optimizations/optimizer.py
+++ DLFrameWork/optimizations/optimizer.py
@@ -1,8 +1,7 @@
 from . import Momentum , Adam
 
 class Optimizer:
-    pass
-    def __init__(self,parameters,optimiType={'GD':True},LayersLen=0,grads=[],lr=lr):
+    def __init__(self,parameters,optimiType={'GD':True},LayersLen=0,grads=[],lr=0):
         self.optimiType = optimiType
         self.grads = grads 
         self.lr = lr
@@ -10,12 +9,12 @@ class Optimizer:
         self.parameters = parameters
 
     def normalGD(self):
-        L = self.LayersLen  # number of layers in the neural network
+        L = self.LayersLen #   # number of layers in the neural network
         # Update rule for each parameter. Use a for loop.
-        for l in range(1,L+1):
+        for l in range(L):
             self.parameters["W" + str(l+1)] = self.parameters["W" + str(l+1)] - self.lr * self.grads['dW'+str(l+1)]
             self.parameters["b" + str(l+1)] = self.parameters["b" + str(l+1)] - self.lr * self.grads['db'+str(l+1)]
-        grads.clear()
+        # grads.clear()
         return self.parameters     
         # pass
         
@@ -25,6 +24,7 @@ class Optimizer:
                 # return Backward.StaticParamUpdate(self.LayersLen,parameters=self.parameters,grads=grads,learning_rate=lr)
             elif 'Adam' in self.optimiType and self.optimiType['Adam']:
                 v,s = Adam.initialize_adam(parameters=self.parameters,layerlen=self.LayersLen)
-                return Adam.update_parameters(layerlen=self.LayersLen,v=v,s=s,grads=grads,parameters=parameters,learning_rate=self.lr)
-            elif 'Momeuntum' in optimiType and optimiType['Momeuntum']:
-                pass
+                return Adam.update_parameters(layerlen=self.LayersLen,v=v,s=s,grads=self.grads,parameters=self.parameters,learning_rate=self.lr)
+            elif 'Momeuntum' in self.optimiType and self.optimiType['Momeuntum']:
+                v = Momentum.initialize_velocity(parameter=self.parameters,layerlen=self.LayersLen)
+                return Momentum.update(layerlen=self.LayersLen,v=v,parameter=self.parameters,lr=self.lr)
