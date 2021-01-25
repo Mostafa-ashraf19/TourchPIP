58a8f90d2d21e728341d56bc25b1d97f254b7327
Mostafa-Ashraf19 <mostaphaashraf19@gmail.com>
2021-01-25 07:18:55 +0200

    init

--- DLFrameWork/__init__.py
+++
@@ -1,7 +0,0 @@
-# from .backward import Backward
-# from .forward import #layer_activations,_params,layers_num_arr
-
-# layer_activations.clear()
-# _params.clear()
-# layers_num_arr.clear()
-# from . import *
