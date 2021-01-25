26f6109a2be214999ee02e2e9a65fbc3f9fd3c9f
Mostafa-Ashraf19 <mostaphaashraf19@gmail.com>
2021-01-25 06:53:11 +0200

    SoftmaxCrossEntropy

--- DLFrameWork/losses/SoftmaxCrossEntropy.py
+++ DLFrameWork/losses/SoftmaxCrossEntropy.py
@@ -9,37 +9,45 @@ class SoftmaxCrossEntropy:
 
     @staticmethod
     def _Cost(prediction,label):
-
         lossval=0
-
         for i in range (label.shape[1]):
 
             ind=label[0,i]
-            y_label=prediction[ind][i]
-            lossval+=-np.log(y_label)
+            y_label= prediction[ind][i]
+            if y_label != 0:
+                lossval+=-np.log(y_label)
 
         cost = lossval/label.shape[1]
 
         return cost
+       
         
-      
+    # @staticmethod
+    # def _Grad(prediction,label) :   
+    #     dl_dy=np.zeros_like(prediction)
+
+    #     for i in range (label.shape[1]):
+
+    #         ind=label[0,i]
+            
+    #         y_label=prediction[ind][i]
+            
+    #         dl_dy[:,i]=-1/y_label
+
+        
+
+    #     return   dl_dy  
         
     @staticmethod
     def _Grad(prediction,label) :
-        
-        
         dl_dy=np.zeros_like(prediction)
 
         for i in range (label.shape[1]):
-            
-
             ind=label[0,i]
 
             dl_dy[:,i]=prediction[:, i]
 
             dl_dy[ind,i]= prediction[ind, i]-1
-
-        
 
         return   dl_dy
 
