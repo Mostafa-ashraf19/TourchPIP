2a1e2206e41828b46637257f160fbad7f0793b4d
Mostafa-Ashraf19 <mostaphaashraf19@gmail.com>
2021-01-25 07:21:46 +0200

    init

--- DLFrameWork/forward/__init__.py
+++ DLFrameWork/forward/__init__.py
@@ -1,8 +1,4 @@
 from .Activaions import Sigmoid, ReLU, Identity, Tanh, Softmax
-from .Layers import Layer_Dense# , _params, layer_activations,layers_num_arr,_ids
+from .Layers import Layer_Dense
 from .NetWork import NetWork
 from .Linear import Linear
-
-# layer_activations.clear()
-# _params.clear()
-# layers_num_arr.clear()
